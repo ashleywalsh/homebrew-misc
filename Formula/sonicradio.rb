41ddd2affd66e8622f52bfb509a2ce40595137f7
Ashley Walsh <ashleywalsh@gmail.com>
2025-03-16 14:53:41 +1100

    sonicradio: use main branch for head

--- Formula/sonicradio.rb
+++ Formula/sonicradio.rb
@@ -2,7 +2,7 @@ class Sonicradio < Formula
   desc "TUI radio player making use of Radio Browser API and Bubbletea"
   homepage "https://github.com/dancnb/sonicradio"
   license "MIT"
-  head "https://github.com/dancnb/sonicradio.git"
+  head "https://github.com/dancnb/sonicradio.git", branch: "main"
 
   depends_on "go" => :build
   depends_on "ffmpeg"
